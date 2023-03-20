e68ddd4ef1711c62c4ae51da28086dc37368ce76
Thomas Cristina de Carvalho <thomas@jecode.dev>
2023-03-20 00:35:15 -0400

    Add vercel edge adapter
    Update env vars to work with vercel edge
    
    
    Fix typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,12 +1,12 @@
 import { configSchema } from "./schemas";
 
 const defineConfig = {
-  shopifyShop: import.meta.env.PUBLIC_SHOPIFY_SHOP,
+  shopifyShop: import.meta.env.PUBLIC_VERCEL_SHOPIFY_SHOP,
   publicShopifyAccessToken: import.meta.env
-    .PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+    .PUBLIC_VERCEL_SHOPIFY_PUBLIC_STOREFRONT_ACCESS_TOKEN,
   privateShopifyAccessToken: import.meta.env
-    .PRIVATE_SHOPIFY_STOREFRONT_ACCESS_TOKEN
-    ? import.meta.env.PRIVATE_SHOPIFY_STOREFRONT_ACCESS_TOKEN
+    .PUBLIC_VERCEL_SHOPIFY_PRIVATE_STOREFRONT_ACCESS_TOKEN
+    ? import.meta.env.PUBLIC_VERCEL_SHOPIFY_PRIVATE_STOREFRONT_ACCESS_TOKEN
     : "",
   apiVersion: "2023-01",
 };

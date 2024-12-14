844cb4ae922f018a3b6ad44fa58d820233a497a1
Thomas Cristina de Carvalho <thomas@frontvibe.com>
2024-12-14 11:40:33 -0500

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
